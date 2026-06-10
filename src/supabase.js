745267c3b072583efc381b552a02b270bda5e0ef
BananenCode <wladislavbaron@outlook.de>
2026-06-10 18:56:41 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const url = 'https://kkvyfreglyrjgrrpvtj.supabase.co'
+const url = 'https://kkvyfregqlyrjgrrpvtj.supabase.co'
 const key = 'sb_publishable_cGn4hNURfnVzjzxffGrvIw_z09RPf9b'
 
 export const supabase = createClient(url, key)
